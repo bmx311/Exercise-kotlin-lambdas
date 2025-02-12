e66d31217c247e67cd55fcceb6956e51154f9191
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-12 15:32:06 +0300

    added part 1 to part 4

---
+++ Lambda Functions.kt
@@ -0,0 +1,20 @@
+//Lambda Functions
+fun main(){
+    //function calls in main
+    greetSeaCreatures()
+    println(findPearl(6))
+    println(divideTreasure(1000.0, 5.0))
+    println(calculateDepth(1500,800))
+}
+
+//part 1
+val greetSeaCreatures: () -> Unit = { println("Hello, Deep sea Adventurers") }
+
+//part 2
+val findPearl: (Int) -> Int = { numPearl -> numPearl * numPearl }
+
+//part 3
+val divideTreasure: (Double, Double) -> Double = { treasureTotal, numExplorer -> treasureTotal / numExplorer }
+
+//part 4
+val calculateDepth: (Int, Int) -> Int = { loc1, loc2 -> loc1 - loc2 }
